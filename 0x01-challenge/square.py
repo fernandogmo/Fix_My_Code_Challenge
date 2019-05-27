8c381e5328a405617369c64d4b20eeb5772df5eb
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-27 02:42:21 +0000

    Tried to save the badly-designed square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
+""" Module (sloppily) defining a `Square` class """
 
-class square():
-    
+
+class Square():
+    """ `Square` class """
+
+    """ default dimensions """
     width = 0
     height = 0
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width, height):
+        """ Ensure dimensions match """
+        if width == height:
+            self.width = width
+            self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of Square instance """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
